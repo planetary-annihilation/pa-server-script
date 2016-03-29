e987924b17a602607ecc8ffdd8afa936ca0e73b5
MikeyH <mikeyh@users.noreply.github.com>
2016-03-30 02:13:52 +1300

    Added support for custom commanders
    - added support for custom commanders in server mods via shadow of
    commander_list.json
    - added update support to commander_manager.js for reloading of
    commander_list.json
    - added reloading of commanders to lobby.js when mod data is updated
    - changed criteria for validation of commanders to exclude unknown
    commanders
    - no change to validation of known commanders

--- server-script/lobby/commander_manager.js
+++ server-script/lobby/commander_manager.js
@@ -5,24 +5,48 @@ var file_utils = require('file_utils');
 function commanderSpec(name) { return '/pa/units/commanders/' + name + '/' + name + '.json'; }
 var default_commanders = _.map(['raptor_centurion', 'raptor_rallus', 'tank_aeson', 'quad_osiris', 'raptor_nemicus', 'imperial_invictus'], commanderSpec)
 
-var commanders = file_utils.loadJsonBlocking('/pa/units/commanders/commander_list.json');
-if (_.has(commanders, 'commanders'))
-    commanders = commanders.commanders;
-else
-    commanders = [];
-
+var commanders = [];
 var specObjectNameMap = {};
-var commandersRemaining = commanders.length;
-_.forEach(commanders, function(commander) {
-    var commanderData = file_utils.loadJsonBlocking(commander);
-    if (_.has(commanderData, 'catalog_object_name'))
-        specObjectNameMap[commander] = commanderData.catalog_object_name;
-});
-var objectSpecPathMap = _.invert(specObjectNameMap);
+var objectSpecPathMap = {};
+var knownCommanderSpecObjectNameMap = {};
 
+function updateCommanders() {
+    commanders = file_utils.loadJsonBlocking('/pa/units/commanders/commander_list.json');
+
+    if (_.has(commanders, 'commanders'))
+        commanders = commanders.commanders;
+    else
+        commanders = [];
+
+    specObjectNameMap = {};
+
+    _.forEach(commanders, function(commander) {
+        var commanderData = file_utils.loadJsonBlocking(commander);
+        if (_.has(commanderData, 'catalog_object_name'))
+            specObjectNameMap[commander] = commanderData.catalog_object_name;
+    });
+    
+    // first update is known commanders
+
+    if ( _.size(knownCommanderSpecObjectNameMap) == 0) {
+        knownCommanderSpecObjectNameMap = specObjectNameMap;
+    }
+
+    objectSpecPathMap = _.invert(specObjectNameMap);
+}
+
+updateCommanders();
 
 function CommanderManager() {
     self = this;
+
+    self.update = function() {
+        updateCommanders();
+    }
+
+    self.isKnownCommanderSpec = function(spec) {
+        return !!knownCommanderSpecObjectNameMap[ spec ];    
+    };
 
     self.getCommanderObjectName = function (spec) {
         if (_.has(specObjectNameMap, spec))
